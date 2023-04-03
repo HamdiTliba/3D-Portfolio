d29947882168774bfb8fe332215f934ad2239142
HamdiTliba <hamditliba.go@gmail.com>
2023-04-03 15:39:15 +0100

    add Loader

--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -1,9 +1,30 @@
-import React from 'react'
+import { Html, useProgress } from "@react-three/drei";
+import React from "react";
 
-const Loader = () => {
+const CanvasLoader = () => {
+  const { progress } = useProgress();
   return (
-    <div>Loader</div>
-  )
-}
+    <Html
+      as="div"
+      center
+      style={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        flexDirection: "column",
+      }}>
+      <span className="canvas-loader top-4"></span>
+      <p
+        style={{
+          fontSize: "13px",
+          color: "#F1F1F1",
+          fontWeight: "500",
+          marginTop: "40",
+        }}>
+        {progress.toFixed(2)}%
+      </p>
+    </Html>
+  );
+};
 
-export default Loader
+export default CanvasLoader;
